de14f0752466bb7c3a5e7ebb28dd53745ac091a7
RUbaba Hajiyeva <rubabahajieva@gmail.com>
2022-09-25 23:47:54 +0400

    learning variables

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        double firstDouble = 20.00d;
+        double secondDouble = 80.00d;
+        double totalDouble = (firstDouble + secondDouble) * 100.00;
+        System.out.println("My values total = " + totalDouble);
+        double firstRemainder = totalDouble % 40.00d;
+        System.out.println("firstRemainder = " + firstRemainder);
+        boolean newBoolean = (firstRemainder == 0.00d) ? true : false;
+        System.out.println("New Boolean = " + newBoolean);
+        if (newBoolean = false) {
+            System.out.println("Got some remainder");
+        }
+    }
+}
